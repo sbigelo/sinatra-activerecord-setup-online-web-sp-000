6dc72d6ca3010278437bf7208ce6da82ccbc5c72
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-14 21:36:02 +0000

    Automatically backed up by Learn

--- db/migrate/20210114213330_create_dogs.rb
+++ db/migrate/20210114213330_create_dogs.rb
@@ -1,14 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
 
-  def up
-    create_table :dogs do |t|
-     t.string :name
-     t.string :breed
+  def change
+     create_table :dogs do |t|
+       t.string :name
+       t.string :breed
+     end
    end
-  end
-
-  def down
-    drop_table :dogs
-  end
 
 end
